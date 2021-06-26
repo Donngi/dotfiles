dbf8dd5af29486d9a23f891aa288c44c4d857d4b
Jimon-s <22317839+Jimon-s@users.noreply.github.com>
2021-06-26 22:05:48 +0900

    Add Homebrew

---
+++ setup/init/init_homebrew.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+if ! command -v brew &> /dev/null
+then
+    echo "Install Homebrew..."
+    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+fi
+
+echo "Start brew bundle..."
+brew bundle --file "$HOME/.dotconfig/.homebrew/Brewfile"
