abdb170dbd50ed7155479a8ff2100c880e5acaee
Jimon-s <22317839+Jimon-s@users.noreply.github.com>
2021-07-03 21:14:46 +0900

    Delete prezto modules

--- .dotconfig/zsh/rc/plugin.zsh
+++ .dotconfig/zsh/rc/plugin.zsh
@@ -9,16 +9,3 @@ zinit light-mode for \
     zinit-zsh/z-a-as-monitor \
     zinit-zsh/z-a-patch-dl \
     zinit-zsh/z-a-bin-gem-node
-
-# ------------------------------------------------------------------------
-# Prezto
-# ------------------------------------------------------------------------
-
-# zinit snippet PZT::modules/environment/init.zsh
-# zinit snippet PZT::modules/terminal/init.zsh
-# zinit snippet PZT::modules/editor/init.zsh
-# zinit snippet PZT::modules/history/init.zsh
-# zinit snippet PZT::modules/directory/init.zsh
-# zinit snippet PZT::modules/spectrum/init.zsh
-# zinit snippet PZT::modules/utility/init.zsh
-# zinit snippet PZT::modules/completion/init.zsh
