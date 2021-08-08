7b75f9a010b01e5676eccc68b86b52e204b1f142
Jimon-s <22317839+Jimon-s@users.noreply.github.com>
2021-08-08 16:04:52 +0900

    Add nodebrew

--- .dotconfig/zsh/rc/xxenv.zsh
+++ .dotconfig/zsh/rc/xxenv.zsh
@@ -16,3 +16,6 @@ export GOENV_ROOT=$HOME/.goenv
 export PATH=$GOENV_ROOT/bin:$PATH
 eval "$(goenv init - --no-rehash)"
 export PATH=$GOPATH/bin:$PATH
+
+# nodebrew
+export PATH=$PATH:$HOME/.nodebrew/current/bin
