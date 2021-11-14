834762d4a65b07fbdb4a182b86eda68b6eaee584
Jimon-s <22317839+Jimon-s@users.noreply.github.com>
2021-11-14 16:11:36 +0900

    Add homebrew path to .zprofile

--- Makefile
+++ Makefile
@@ -1,7 +1,7 @@
 all-in-one: init-all deploy-all
 
 deploy-all:
-	@$(foreach val, $(wildcard ./setup/deploy/*.sh), bash $(val);)
+	@$(foreach val, $(wildcard ./setup/deploy/deploy_*.sh), bash $(val);)
 
 deploy-home:
 	@$(foreach val, $(wildcard ./setup/deploy/deploy_home.sh), bash $(val);)
--- setup/init/init_homebrew.sh
+++ setup/init/init_homebrew.sh
@@ -9,6 +9,8 @@ if ! command -v brew &> /dev/null
 then
     echo "Install Homebrew ..."
     /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+    # deploy完了までの間にhomebrewのpathを通すため、一時的書き込む(deploy phaseで上書きされる)
+    echo 'eval "$(/opt/homebrew/bin/brew shellenv)"' >> $HOME/.zprofile
     echo ""
 else
     echo "Homebrew is already installed."
