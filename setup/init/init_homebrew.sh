8cecd45857c33ebc24b7d222248f9319291bd375
Jimon-s <22317839+Jimon-s@users.noreply.github.com>
2021-11-14 16:33:11 +0900

    Add homebrew path to .zprofile

--- setup/init/init_homebrew.sh
+++ setup/init/init_homebrew.sh
@@ -9,6 +9,10 @@ if ! command -v brew &> /dev/null
 then
     echo "Install Homebrew ..."
     /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+    # deploy完了までの間にhomebrewのpathを通すため、一時的に.zprofileにpathを書き込む(deploy phaseで上書きされる)
+    echo "Write homebrew path to .zprofile"
+    echo 'eval "$(/opt/homebrew/bin/brew shellenv)"' >> $HOME/.zprofile
+    source $HOME/.zprofile
     echo ""
 else
     echo "Homebrew is already installed."
