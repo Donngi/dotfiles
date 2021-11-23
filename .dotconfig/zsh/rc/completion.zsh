0cdfd72188e5bd844555de71d780cf5750a5cf20
Jimon-s <22317839+Jimon-s@users.noreply.github.com>
2021-11-23 16:37:43 +0900

    Remove zsh-completions and activate homebrew completions

--- .dotconfig/zsh/rc/completion.zsh
+++ .dotconfig/zsh/rc/completion.zsh
@@ -10,7 +10,7 @@
 
 # 補完有効化
 if type brew &>/dev/null; then
-    FPATH=$(brew --prefix)/share/zsh-completions:$FPATH
+    FPATH="$(brew --prefix)/share/zsh/site-functions:${FPATH}"
 fi
 autoload -Uz compinit && compinit
 
