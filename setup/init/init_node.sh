f330534c85dd83999f033803577d0b32f89d7706
Jimon-s <22317839+Jimon-s@users.noreply.github.com>
2023-03-21 11:08:31 +0900

    Install latest node when installing volta

--- setup/init/init_node.sh
+++ setup/init/init_node.sh
@@ -1,3 +1,9 @@
 # Install volta
 # https://docs.volta.sh/guide/getting-started
 curl https://get.volta.sh | bash
+
+export VOLTA_HOME="$HOME/.volta"
+export PATH="$VOLTA_HOME/bin:$PATH"
+
+volta install node
+node -v
