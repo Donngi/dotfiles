85acd4fba37c09d07aeef70be4114324ed3998b3
Jimon-s <22317839+Jimon-s@users.noreply.github.com>
2023-04-05 22:06:30 +0900

    Feat: auto update nvim-tree when select a file by telescope

--- .dotconfig/nvim/init.lua
+++ .dotconfig/nvim/init.lua
@@ -1,7 +1,8 @@
 require("base")
 require("plugins")
 require("keymaps")
-require("nvim_tree")
-require("nvim_vscode")
+require("plugin_nvim_tree")
+require("plugin_nvim_vscode")
+require("plugin_telescope")
 
 vim.cmd("autocmd BufWritePost plugins.lua PackerCompile") -- Auto compile when there are changes in plugins.lua
