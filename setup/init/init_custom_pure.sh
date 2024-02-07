178eb30bee7833e95f8efb58e986c6c143e7aafd
Donngi <22317839+Donngi@users.noreply.github.com>
2024-02-08 06:48:21 +0900

    Enable pure install command also be used to perform updates

--- setup/init/init_custom_pure.sh
+++ setup/init/init_custom_pure.sh
@@ -6,5 +6,13 @@ echo "# ------------------------------------"
 echo ""
 
 mkdir -p "$HOME/.zsh"
-git clone https://github.com/Donngi/pure.git "$HOME/.zsh/pure"
+if [ -d "$HOME/.zsh/pure" ]; then
+    echo "Already exists $HOME/.zsh/pure. Update repository with git pull"
+    cd "$HOME/.zsh/pure"
+    git pull
+    echo "Done: git pull"
+else
+    echo "Not exists $HOME/.zsh/pure. git clone"
+    git clone https://github.com/Donngi/pure.git "$HOME/.zsh/pure"
+fi
 
