623c175a747033fc6c3943ec2a5c54838f6146ac
Donngi <22317839+Donngi@users.noreply.github.com>
2024-04-09 05:53:24 +0900

    VSCode: keybindings.jsonをosごとに分割

--- setup/mac_os/deploy/deploy_vscode.sh
+++ setup/mac_os/deploy/deploy_vscode.sh
@@ -4,6 +4,6 @@ script_dir=$(cd $(dirname $0); pwd)
 
 target="VSCode"
 deploy_dir="$HOME/Library/Application Support/Code/User"
-source_dir="$ZDOTDIR/.dotconfig/vscode"
+source_dir="$ZDOTDIR/.dotconfig/vscode/mac_os"
 files="keybindings.json"
 source "$script_dir/util_deploy.sh" "$target" "$deploy_dir" "$source_dir" "$files"
