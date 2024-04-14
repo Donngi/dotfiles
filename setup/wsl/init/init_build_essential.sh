caff0a089c0375dbdccc73c8ebe710248065a43e
Donngi <22317839+Donngi@users.noreply.github.com>
2024-04-15 06:32:05 +0900

    wsl: Add install script for build essential

---
+++ setup/wsl/init/init_build_essential.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+echo "# ------------------------------------"
+echo "# START: Install build-essential"
+echo "# ------------------------------------"
+
+sudo apt-get update -y
+sudo apt-get install -y build-essential
