1db62214a22af2dbae99e60082b96c44e52bed11
Donngi <22317839+Donngi@users.noreply.github.com>
2024-04-15 06:41:28 +0900

    wsl: Add install script for docker

---
+++ setup/wsl/init/init_docker.sh
@@ -0,0 +1,31 @@
+#!/bin/sh
+
+echo "# ------------------------------------"
+echo "# START: Install docker"
+echo "# ------------------------------------"
+
+# https://docs.docker.com/engine/install/ubuntu/#installation-methods
+
+# Add Docker's official GPG key:
+sudo apt-get update
+sudo apt-get install ca-certificates curl
+sudo install -m 0755 -d /etc/apt/keyrings
+sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
+sudo chmod a+r /etc/apt/keyrings/docker.asc
+
+# Add the repository to Apt sources:
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
+  $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
+  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt-get update
+
+# sudoなしでdockerを利用できるようにする
+# https://docs.docker.com/engine/install/linux-postinstall/
+sudo groupadd docker
+sudo usermod -aG docker $USER
+newgrp docker
+
+# boot時にdockerを立ち上げ
+sudo systemctl enable docker.service
+sudo systemctl enable containerd.service
