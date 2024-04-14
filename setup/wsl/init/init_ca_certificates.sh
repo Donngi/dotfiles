ef09a5ba7cd75d459894a16f7490ea0c381a4425
Donngi <22317839+Donngi@users.noreply.github.com>
2024-04-15 06:33:39 +0900

    wsl: Add install script for ca certificates

---
+++ setup/wsl/init/init_ca_certificates.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+echo "# ------------------------------------"
+echo "# START: Install ca-certificates"
+echo "# ------------------------------------"
+
+sudo apt-get update -y
+sudo apt-get install -y ca-certificates
+
