37f6357a8f249a393a0edd0e7d64aa7f0b6be5bd
Donngi <22317839+Donngi@users.noreply.github.com>
2024-04-19 06:28:11 +0900

    go: 利用バージョンの指定をデフォルトOFFにした

--- .dotconfig/zsh/rc/xxenv.zsh
+++ .dotconfig/zsh/rc/xxenv.zsh
@@ -12,11 +12,13 @@ eval "$(pyenv init - --no-rehash)"
 eval "$(rbenv init - --no-rehash)"
 
 # go
-export GOROOT=`go1.22.1 env GOROOT`
-export PATH=$GOROOT/bin:$PATH
-
 export GOPATH=$HOME/.go
 export PATH=$GOPATH/bin:$PATH
+
+# goの利用するバージョンを明示的に指定したい場合、
+# https://go.dev/doc/manage-install を参考にインストールしたうえで、明示的にGOROOT
+# export GOROOT=`go1.22.1 env GOROOT` 
+# export PATH=$GOROOT/bin:$PATH
 
 # nodebrew
 export PATH=$PATH:$HOME/.nodebrew/current/bin
