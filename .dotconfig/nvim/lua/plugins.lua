60ae33ffd68d68a546c1c14563dc9f50cce8badb
Donngi <22317839+Donngi@users.noreply.github.com>
2026-02-07 22:02:55 +0900

    nvim: ターミナルカラーを Ghostty の Iceberg Dark に統一
    toggleterm のターミナル配色が Ghostty と異なっていたため、
    Neovim のターミナルカラーを Iceberg Dark パレットで上書きする。

--- .dotconfig/nvim/lua/plugins.lua
+++ .dotconfig/nvim/lua/plugins.lua
@@ -87,6 +87,24 @@ require("lazy").setup({
       vim.o.background = 'dark'
       require('vscode').setup({})
       require('vscode').load()
+
+      -- ターミナルカラーを Iceberg Dark に合わせる（Ghostty と統一）
+      vim.g.terminal_color_0  = "#161821"  -- black
+      vim.g.terminal_color_1  = "#e27878"  -- red
+      vim.g.terminal_color_2  = "#b4be82"  -- green
+      vim.g.terminal_color_3  = "#e2e2bf"  -- yellow (Ghostty カスタム)
+      vim.g.terminal_color_4  = "#84a0c6"  -- blue
+      vim.g.terminal_color_5  = "#a093c7"  -- magenta
+      vim.g.terminal_color_6  = "#89b8c2"  -- cyan
+      vim.g.terminal_color_7  = "#c6c8d1"  -- white
+      vim.g.terminal_color_8  = "#6b7089"  -- bright black
+      vim.g.terminal_color_9  = "#e98989"  -- bright red
+      vim.g.terminal_color_10 = "#c0ca8e"  -- bright green
+      vim.g.terminal_color_11 = "#e9b189"  -- bright yellow
+      vim.g.terminal_color_12 = "#91acd1"  -- bright blue
+      vim.g.terminal_color_13 = "#ada0d3"  -- bright magenta
+      vim.g.terminal_color_14 = "#95c4ce"  -- bright cyan
+      vim.g.terminal_color_15 = "#d2d4de"  -- bright white
     end,
   },
 
@@ -174,6 +192,7 @@ require("lazy").setup({
       size = 20,
       open_mapping = [[<C-\>]],
       direction = "float",
+      shade_terminals = false,
       float_opts = {
         border = "curved",
       },
